5df12861919287549b28f76f5e358bef5f0d378d
altronis <neji1011@naver.com>
2018-03-14 17:38:57 -0400

    changed URLs

--- maxperformance/MAXpp/urls.py
+++ maxperformance/MAXpp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'MAXpp'
 urlpatterns = [
-    url(r'/auth/$', views.auth_view, name='auth'),
-    url(r'/scores/$', views.scores_view, name='scores'),
+    url(r'^/auth/$', views.auth_view, name='auth'),
+    url(r'^/scores/$', views.scores_view, name='scores'),
     url(r'^$', views.index, name='index'),
 ]
