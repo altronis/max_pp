6e93f73174af869459e7efae9064f3df791c0289
altronis <neji1011@naver.com>
2018-03-14 16:55:00 -0400

    changed wsgi

--- maxperformance/maxperformance/wsgi.py
+++ maxperformance/maxperformance/wsgi.py
@@ -1,6 +1,8 @@
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maxperformance.settings")
 
